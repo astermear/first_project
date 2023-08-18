b20143bd9fd971963a0340466eeb6cfb2b8e2f6f
aster <mear.aster@gmail.com>
2023-08-18 22:59:04 +0300

    create personalMovieDB

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let numberOfFilms = prompt("Сколько фильмов вы посмотрели?", "");
+let numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
