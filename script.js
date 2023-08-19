d8ea6ecee06427fb49280a592f6892c09de9a69e
aster <mear.aster@gmail.com>
2023-08-19 03:42:25 +0300

    add input checker

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+let numberOfFilms = "";
+do
+{
+    numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+}
+while(!numberOfFilms);
+
 let personalMovieDB = {
     count : numberOfFilms,
     movies : {},
@@ -7,11 +13,24 @@ let personalMovieDB = {
     privat : false
 };
 
-
 for (let i = 0; i < 2; i++)
 {
-    let lastFilm = prompt("Один из последних посмотренных фильмо?", '');
-    let rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
+    let lastFilm, rateLastFilm;
+
+    lastFilm = prompt("Один из последних посмотренных фильмов?", "");
+    while(!lastFilm || lastFilm > 50)
+    {
+        alert("Ошибка!");
+        lastFilm = prompt("Один из последних посмотренных фильмов?", "");
+    }
+
+    rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
+    while(!rateLastFilm || rateLastFilm > 10 || rateLastFilm < 0)
+    {
+        alert("Ошибка!");
+        rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
+    }
+    
     personalMovieDB.movies[lastFilm] = rateLastFilm;
 }
 console.log(personalMovieDB);
