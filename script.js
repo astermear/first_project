f766830857a5abef48578585e0dde243d000187d
aster <mear.aster@gmail.com>
2023-08-21 03:32:38 +0300

    wrap code to functions

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let numberOfFilms = "";
-do
-{
+let numberOfFilms;
+
+
+function start() {
+    do
+    {
     numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+    }
+    while(!numberOfFilms || isNaN(numberOfFilms) || numberOfFilms < 0);
 }
-while(!numberOfFilms || numberOfFilms < 0);
+
+start();
 
 let personalMovieDB = {
     count : numberOfFilms,
@@ -13,41 +19,52 @@ let personalMovieDB = {
     privat : false
 };
 
-if (personalMovieDB <= 10)
-    {
-        alert("Просмотрено довольно мало фильмов!");
-    }
-else if(personalMovieDB <= 30)
-    {
-        alert("Вы классический зритель!");
-    }
-else
-    {
-        alert("Вы киноман!");
-    }
 
 
-for (let i = 0; i < 2; i++)
-{
-    let lastFilm, rateLastFilm;
 
-    lastFilm = prompt("Один из последних посмотренных фильмов?", "");
-    while(!lastFilm || lastFilm > 50)
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++)
     {
-        alert("Ошибка!");
+        let lastFilm, rateLastFilm;
+
         lastFilm = prompt("Один из последних посмотренных фильмов?", "");
-    }
+        while(!lastFilm || lastFilm > 50)
+        {
+            alert("Ошибка!");
+            lastFilm = prompt("Один из последних посмотренных фильмов?", "");
+        }
 
-    rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
-    while(!rateLastFilm || rateLastFilm > 10 || rateLastFilm < 0)
-    {
-        alert("Ошибка!");
         rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
+        while(!rateLastFilm || rateLastFilm > 10 || rateLastFilm < 0)
+        {
+            alert("Ошибка!");
+            rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
+        }
+        
+        personalMovieDB.movies[lastFilm] = rateLastFilm;
     }
-    
-    personalMovieDB.movies[lastFilm] = rateLastFilm;
 }
-console.log(personalMovieDB);
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB <= 10)
+        {
+            alert("Просмотрено довольно мало фильмов!");
+        }
+    else if(personalMovieDB <= 30)
+        {
+            alert("Вы классический зритель!");
+        }
+    else
+        {
+            alert("Вы киноман!");
+        }
+
+}
+
+
+
 
 
 
