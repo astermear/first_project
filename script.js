b8bad7ef7ed0d9c5664f15a1e09aee995706bbf7
aster <mear.aster@gmail.com>
2023-08-22 12:29:01 +0300

    mod personalMovieDB, add methods

--- script.js
+++ script.js
@@ -1,54 +1,42 @@
-let numberOfFilms;
 
-
-function start() {
-    do
-    {
-    numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
-    }
-    while(!numberOfFilms || isNaN(numberOfFilms) || numberOfFilms < 0);
-}
-
-start();
 
 let personalMovieDB = {
-    count : numberOfFilms,
+    count : 0,
     movies : {},
     actors : {},
     genres : [],
-    privat : false
-};
-
-
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++)
+    privat : false,
+    start: function () {
+    do 
     {
-        let lastFilm, rateLastFilm;
+        return +prompt("Сколько фильмов вы посмотрели?", "");
+    } 
+    while(!numberOfFilms || isNaN(numberOfFilms) || numberOfFilms < 0);
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++)
+            {
+                let lastFilm, rateLastFilm;
 
-        lastFilm = prompt("Один из последних посмотренных фильмов?", "");
-        while(!lastFilm || lastFilm > 50)
-        {
-            alert("Ошибка!");
-            lastFilm = prompt("Один из последних посмотренных фильмов?", "");
-        }
+                lastFilm = prompt("Один из последних посмотренных фильмов?", "");
+                while(!lastFilm || lastFilm > 50)
+                {
+                    alert("Ошибка!");
+                    lastFilm = prompt("Один из последних посмотренных фильмов?", "");
+                }
 
-        rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
-        while(!rateLastFilm || rateLastFilm > 10 || rateLastFilm < 0)
-        {
-            alert("Ошибка!");
-            rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
-        }
-        
-        personalMovieDB.movies[lastFilm] = rateLastFilm;
-    }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB <= 10)
+                rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
+                while(!rateLastFilm || rateLastFilm > 10 || rateLastFilm < 0)
+                {
+                    alert("Ошибка!");
+                    rateLastFilm = +prompt("Как вы его оцените(от 1 до 10)?", "");
+                }
+                
+                personalMovieDB.movies[lastFilm] = rateLastFilm;
+            }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB <= 10)
         {
             alert("Просмотрено довольно мало фильмов!");
         }
@@ -60,23 +48,26 @@ function detectPersonalLevel() {
         {
             alert("Вы киноман!");
         }
-
-}
-
-function showMyDB() {
-    if(!personalMovieDB.privat) {
-        console.log(personalMovieDB);
+    },
+    showMyDB: function() {
+        if(!personalMovieDB.privat) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        let answerUser;
+        for (let i = 1; i <= 3; i++) {
+            answerUser = prompt(`Ваш любимый жанр под номером ${i}?`, "");
+            personalMovieDB.genres[i - 1] = answerUser;
     }
-}
-
-function writeYourGenres() {
-    let answerUser;
-    for (let i = 1; i <= 3; i++) {
-        answerUser = prompt(`Ваш любимый жанр под номером ${i}?`, "");
-        personalMovieDB.genres[i - 1] = answerUser;
     }
-}
+    
+};
 
-
+personalMovieDB.count = personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyDB();
 
 
